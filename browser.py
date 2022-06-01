68f1f9a18d770bab8ce14228fa5bce7657d03fec
zekiyasin <zeki.kocyigit.2002@gmail.com>
2022-06-02 00:12:21 +0300

    fix for instagram update

--- browser.py
+++ browser.py
@@ -12,6 +12,7 @@ class Browser:
 		chrome_options.add_argument('--disable-dev-shm-usage')
   
 		self.browser = webdriver.Chrome("./chromedriver.exe",chrome_options=chrome_options)
+		# self.browser = webdriver.Chrome(executable_path="./chromedriver.exe")
 		self.userName = kb.userName
 		self.password = kb.password
 		Browser.goInstagram(self)
@@ -26,13 +27,13 @@ class Browser:
 		Browser.compare(self,takipçiler,takipler)
 
 	def getFollowers(self):
-		self.browser.find_element_by_xpath('//*[@id=\"react-root\"]/section/main/div/header/section/ul/li[2]/a').click()
+		self.browser.find_elements_by_css_selector("._aacl._aacp._aacu._aacx._aad6._aade")[1].click()
 		time.sleep(4)
 
 		Browser.scrollDown(self)
 		time.sleep(2)
 
-		takipciler = self.browser.find_elements_by_css_selector(".notranslate._0imsa") 
+		takipciler = self.browser.find_elements_by_css_selector(".notranslate._aaek._a6hd") 
 		sayac = 0
 		takipçiler = []
 		for takipci in takipciler:
@@ -52,11 +53,11 @@ class Browser:
 	def get_takip(self,):
 		self.browser.get(self.link+"/"+kb.userName)
 		time.sleep(5)
-		self.browser.find_element_by_xpath('//*[@id=\"react-root\"]/section/main/div/header/section/ul/li[3]/a').click()
+		self.browser.find_elements_by_css_selector("._aacl._aacp._aacu._aacx._aad6._aade")[2].click()
 		time.sleep(4)
 		Browser.scrollDown(self)
 		time.sleep(2)
-		takip_edilenler = self.browser.find_elements_by_css_selector(".notranslate._0imsa")
+		takip_edilenler = self.browser.find_elements_by_css_selector(".notranslate._aaek._a6hd")
 		takipler = []
 		sayac = 0
 		for takip in takip_edilenler:
@@ -94,7 +95,7 @@ class Browser:
 
 	def scrollDown(self):
 		jsKomut = """
-		sayfa = document.querySelector(".isgrP");
+		sayfa = document.querySelector("._aano");
 		sayfa.scrollTo(0,sayfa.scrollHeight);
 		var sayfaSonu = sayfa.scrollHeight;
 		return sayfaSonu;
@@ -107,18 +108,8 @@ class Browser:
 			if son == sayfaSonu:
 				break
 
-
-	def login(self):
-		username = self.browser.find_element_by_name("username")
-		password = self.browser.find_element_by_name("password")
-
-		username.send_keys(kb.userName)
-		password.send_keys(kb.password)
-
-		loginBtn = self.browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')
-		loginBtn.click()
-		time.sleep(4)
-  
+	def verify_2ad(self):
+     
 		verification_code = input("Enter verification code: ")
   
 		time.sleep(60)
@@ -130,6 +121,22 @@ class Browser:
 		verify_button.click()
 		time.sleep(10)
   
+     
+	def login(self):
+		username = self.browser.find_element_by_name("username")
+		password = self.browser.find_element_by_name("password")
+
+		username.send_keys(kb.userName)
+		password.send_keys(kb.password)
+
+		loginBtn = self.browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')
+		loginBtn.click()
+		time.sleep(4)
+  
+		# Browser.verify_2ad(self)
+  
+		time.sleep(10)
 		self.browser.execute_script(f'''window.open("{self.link+"/"+kb.userName}");''')
+		time.sleep(5)
 		self.browser.switch_to.window(self.browser.window_handles[1])
-		time.sleep(3)
+		time.sleep(10)
